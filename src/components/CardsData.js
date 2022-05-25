49fd9fbeb009402ee05fa221401d2d1cec8b7201
lavanya <pothireddylavanya35@gmail.com>
2022-05-25 20:51:18 +0530

    first commit

---
+++ src/components/CardsData.js
@@ -0,0 +1,68 @@
+const Cardsdata = [
+    {
+        id: 1,
+        Name: "The Graphic Novel",
+        imgdata: "https://th.bing.com/th/id/OIP.i-Cq8CQ0JQFS4enJxzLo2gHaJl?w=202&h=262&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        price: 299,
+        rating: "4.0",
+        
+    },
+    {
+        id: 2,
+        Name: "The Hunger",
+        imgdata: "https://th.bing.com/th/id/OIP.X7SSAy6Y65ClmNkQrT6zEgHaLL?w=202&h=305&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        price: 299,
+        rating: "4.5",
+    },
+    {
+        id: 3,
+        Name: "Hidden Sun",
+        imgdata: "https://th.bing.com/th/id/OIP.1Mk6kNpE7aLrO89jSv4T8AHaLO?w=202&h=305&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        price: 199,
+        rating: "4.2",
+    },
+    {
+        id: 4,
+        Name: "The Time Machine",
+        imgdata: "https://th.bing.com/th/id/OIP.13mk9Y7qwwuSeJfMjgRpRwHaLh?w=200&h=311&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        price: 299,
+        rating: "4.8",
+    },
+    {
+        id: 5,
+        Name: "Your Book Title",
+        imgdata: "https://th.bing.com/th/id/OIP.VeqHC7qQC2P6oPdXXyqkMwHaLH?w=202&h=303&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        price: 199,
+        rating: "4.0",
+    },
+    {
+        id: 6,
+        Name: "Nature Kingdom",
+        imgdata: "https://th.bing.com/th/id/OIP._b0f0ekkLa0ledT8368SjQHaL2?w=197&h=316&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        price: 299,
+        rating: "4.5",
+    },
+    {
+        id: 7,
+        Name: "Powell Embers of War",
+        imgdata: "https://th.bing.com/th/id/OIP.uwmZbBdUZhKqI1j4yqkT5gHaLS?w=202&h=308&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        price: 399,
+        rating: "4.8",    
+    },
+    {
+        id: 8,
+        Name: "Star of Wonder",
+        imgdata: "https://th.bing.com/th/id/OIP.spSxueKAzEk4laUTdBlhAQHaLH?w=202&h=303&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        price: 199,
+        rating: "4.2",  
+    },
+    {
+        id: 9,
+        Name: "John Grisham",
+        imgdata: "https://th.bing.com/th/id/OIP.B7Jw2qOw6mb4LMeAeWHXDAAAAA?w=201&h=310&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        price: 199,
+        rating: "4.0",
+    },
+];
+
+export default Cardsdata;
